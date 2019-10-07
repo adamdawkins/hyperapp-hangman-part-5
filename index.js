f85353f77983ab1756bdf3838ffc0743022d75c0
Adam Dawkins <adam@dragondrop.uk>
2019-10-07 22:52:27 +0100

    Part 3

---
+++ index.js
@@ -0,0 +1,101 @@
+import {app} from 'hyperapp';
+import {
+  div,
+  h1,
+  h2,
+  ul,
+  li,
+  span,
+  input,
+  label,
+  form,
+  button,
+} from '@hyperapp/html';
+import {targetValue, preventDefault} from '@hyperapp/events';
+
+const mdash = '\u2014';
+const MAX_BAD_GUESSES = 7;
+
+// UTILITIES
+
+const contains = (list, item) => list.indexOf(item) > -1;
+
+// HELPERS
+const isGuessed = (letter, state) => contains(state.guesses, letter);
+const isInWord = (letter, state) => contains(state.word, letter);
+
+const badGuesses = state =>
+  state.guesses.filter(guess => !isInWord(guess, state));
+
+const isVictorious = state =>
+  state.word.every(letter => isGuessed(letter, state));
+
+const isGameOver = state => badGuesses(state).length >= MAX_BAD_GUESSES;
+
+// EFFECTS
+
+// ACTIONS
+
+const GuessLetter = state => ({
+  ...state,
+  guesses: state.guesses.concat([state.guessedLetter]),
+  guessedLetter: '',
+});
+
+const SetGuessedLetter = (state, letter) => ({
+  ...state,
+  guessedLetter: letter,
+});
+
+// VIEWS
+
+const WordLetter = (letter, guessed) =>
+  span({class: 'letter'}, guessed ? letter : mdash);
+
+const Word = state =>
+  h1(
+    {},
+    state.word.map(letter => WordLetter(letter, isGuessed(letter, state))),
+  );
+
+const BadGuesses = state => [
+  h2({}, 'Your Guesses:'),
+  ul(
+    {class: 'guesses'},
+    badGuesses(state).map(guess => li({class: 'guess'}, guess)),
+  ),
+];
+
+const UserInput = letter =>
+  form({onSubmit: preventDefault(GuessLetter)}, [
+    label({}, 'Your guess:'),
+    ,
+    input({
+      class: 'input',
+      onInput: [SetGuessedLetter, targetValue],
+      type: 'text',
+      value: letter,
+      maxlength: 1,
+    }),
+    button({type: 'submit'}, 'Guess!'),
+  ]);
+
+// THE APP
+
+app({
+  init: {
+    word: 'application'.split(''),
+    guesses: [],
+    guessedLetter: '',
+  },
+  view: state =>
+    div(
+      {},
+      isGameOver(state)
+        ? h1({}, `Game Over! The word was "${state.word.join('')}"`)
+        : isVictorious(state)
+        ? [h1({}, 'You Won!'), Word(state)]
+        : [UserInput(state.guessedLetter), Word(state), BadGuesses(state)],
+    ),
+  node: document.getElementById('app'),
+});
