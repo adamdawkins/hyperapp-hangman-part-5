d03b3af156265038a72c0d9102325eb381cb1d11
Adam Dawkins <adam@dragondrop.uk>
2019-10-24 19:50:29 +0100

    ✅ Add HTTP call to random word

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {app} from 'hyperapp';
+import { app } from "hyperapp";
 import {
   div,
   h1,
@@ -9,11 +9,12 @@ import {
   input,
   label,
   form,
-  button,
-} from '@hyperapp/html';
-import {targetValue, preventDefault} from '@hyperapp/events';
+  button
+} from "@hyperapp/html";
+import { get } from "@hyperapp/http";
+import { targetValue, preventDefault } from "@hyperapp/events";
 
-const mdash = '\u2014';
+const mdash = "\u2014";
 const MAX_BAD_GUESSES = 7;
 
 // UTILITIES
@@ -39,63 +40,77 @@ const isGameOver = state => badGuesses(state).length >= MAX_BAD_GUESSES;
 const GuessLetter = state => ({
   ...state,
   guesses: state.guesses.concat([state.guessedLetter]),
-  guessedLetter: '',
+  guessedLetter: ""
 });
 
 const SetGuessedLetter = (state, letter) => ({
   ...state,
-  guessedLetter: letter,
+  guessedLetter: letter
+});
+
+const SetWord = (state, { word }) => ({
+  ...state,
+  word: word.split("")
 });
 
 // VIEWS
 
 const WordLetter = (letter, guessed) =>
-  span({class: 'letter'}, guessed ? letter : mdash);
+  span({ class: "letter" }, guessed ? letter : mdash);
 
 const Word = state =>
   h1(
     {},
-    state.word.map(letter => WordLetter(letter, isGuessed(letter, state))),
+    state.word.map(letter => WordLetter(letter, isGuessed(letter, state)))
   );
 
 const BadGuesses = state => [
-  h2({}, 'Your Guesses:'),
+  h2({}, "Your Guesses:"),
   ul(
-    {class: 'guesses'},
-    badGuesses(state).map(guess => li({class: 'guess'}, guess)),
-  ),
+    { class: "guesses" },
+    badGuesses(state).map(guess => li({ class: "guess" }, guess))
+  )
 ];
 
 const UserInput = letter =>
-  form({onSubmit: preventDefault(GuessLetter)}, [
-    label({}, 'Your guess:'),
+  form({ onSubmit: preventDefault(GuessLetter) }, [
+    label({}, "Your guess:"),
     ,
     input({
-      class: 'input',
+      class: "input",
       onInput: [SetGuessedLetter, targetValue],
-      type: 'text',
+      type: "text",
       value: letter,
-      maxlength: 1,
+      maxlength: 1
     }),
-    button({type: 'submit'}, 'Guess!'),
+    button({ type: "submit" }, "Guess!")
   ]);
 
+const getWord = () =>
+  get({
+    url: `https://adamdawkins.uk/randomword.json`,
+    expect: "json",
+    action: SetWord
+  });
 // THE APP
 
 app({
-  init: {
-    word: 'application'.split(''),
-    guesses: [],
-    guessedLetter: '',
-  },
+  init: [
+    {
+      word: [],
+      guesses: [],
+      guessedLetter: ""
+    },
+    getWord()
+  ],
   view: state =>
     div(
       {},
       isGameOver(state)
-        ? h1({}, `Game Over! The word was "${state.word.join('')}"`)
+        ? h1({}, `Game Over! The word was "${state.word.join("")}"`)
         : isVictorious(state)
-        ? [h1({}, 'You Won!'), Word(state)]
-        : [UserInput(state.guessedLetter), Word(state), BadGuesses(state)],
+        ? [h1({}, "You Won!"), Word(state)]
+        : [UserInput(state.guessedLetter), Word(state), BadGuesses(state)]
     ),
-  node: document.getElementById('app'),
+  node: document.getElementById("app")
 });
